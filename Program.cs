0f71adabb93c19f04c1c3aabc98a653d9d009a97
sanliu <sanliu@blizzard.com>
2022-10-17 22:01:23 -0700

    remove client details

--- Program.cs
+++ Program.cs
@@ -11,24 +11,7 @@ namespace AccessAdminAuditorV3
     {
         static void Main(string[] args)
         {
-            //TaskConfiguration c = new TaskConfiguration();
-            //c.env = Config.Production;
-            //c.endpoint = Config.Asterion;
-            //Tasks.Initialize();
-            //string resp = Tasks.AccountGroupServices.GetAccountGroupInfo_SingleAPICall(c, "075C8E4D-E529-4720-9B6B-A061E2B92E4C");
-            //string resp = Tasks.AuthorizationServices.GetScopeForRole_SingleAPICall(c, "075C8E4D-E529-4720-9B6B-A061E2B92E4C");
-            //Console.WriteLine(resp);
-            //string inputFilePath = Paths.inputFilePath += "COD Group Audit for Ryan Ford.csv";
-            //string outputFileName = "V3GroupAuditingAccountGroup";
-            //Tasks.AccountGroupServices.ExportFullAccountGroupDetailstoCSV_FromReadingFile(c, inputFilePath, outputFileName);
-            //Tasks.AuthorizationServices.GetOAuthClientDetailFromClientId(c, "2f02217feef144ed94daae9fcacaea93");
-            //Tasks.AuthorizationServices.WhoIsUsingMyScope(c, "account.full", "account_full_withKhoa");
-
-            
             App.UI.MainMenu.Run();
-
-            
-
         }
     }
 }
